3305c03329b4b813e5386ba7ad48f6c6ac37b3a6
Ross Gardler <ross.gardler@microsoft.com>
2022-01-24 20:21:28 -0800

    add the sound cotroller script required for the Unka example

---
+++ Scripts/Runtime/SoundController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace WizardsCode.Kalmeer.Wildlife
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class SoundController : MonoBehaviour
+    {
+        [SerializeField, Tooltip("The wingflap sounda, typically a random one will be played on the standard wing downstroke.")]
+        AudioClip[] m_WingFlapClip;
+
+        AudioSource audioSource;
+
+        private void Awake()
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        public void PlaySound()
+        {
+            audioSource.clip = m_WingFlapClip[Random.Range(0, m_WingFlapClip.Length)];
+            audioSource.pitch = Random.Range(0.95f, 1.05f);
+            audioSource.Play();
+        }
+    }
+}
